407152e2766fb117d3406c658bbcc1c5af44626d
Jarad Inniss <jjinniss02@gmail.com>
2026-07-06 18:47:43 +0800

    Added User Management

--- server/frontend/src/components/Login/Login.jsx
+++ server/frontend/src/components/Login/Login.jsx
@@ -41,7 +41,7 @@ const Login = ({ onClose }) => {
   
 
   return (
-    <div>
+    <div style={{ backgroundColor: "#1F1F1F" }}>
       <Header/>
     <div onClick={onClose}>
       <div
@@ -52,18 +52,25 @@ const Login = ({ onClose }) => {
       >
           <form className="login_panel" style={{}} onSubmit={login}>
               <div>
-              <span className="input_field">Username </span>
-              <input type="text"  name="username" placeholder="Username" className="input_field" onChange={(e) => setUserName(e.target.value)}/>
+                <span className="input_field" style={{ color: "#F5F5F5", fontWeight: "bold" }}> Username </span>
+                <input type="text"  name="username" placeholder="Username" className="input_field" onChange={(e) => setUserName(e.target.value)}
+                style={{ border: "none", borderRadius: "10px", backgroundColor: "#F5F5F5", padding: "10px" }}/>
               </div>
+
               <div>
-              <span className="input_field">Password </span>
-              <input name="psw" type="password"  placeholder="Password" className="input_field" onChange={(e) => setPassword(e.target.value)}/>            
+                <span className="input_field" style={{ color: "#F5F5F5", fontWeight: "bold" }}> Password </span>
+                <input name="psw" type="password"  placeholder="Password" className="input_field" onChange={(e) => setPassword(e.target.value)}
+                style={{ border: "none", borderRadius: "10px", backgroundColor: "#F5F5F5", padding: "10px" }}/>            
               </div>
+
               <div>
-              <input className="action_button" type="submit" value="Login"/>
-              <input className="action_button" type="button" value="Cancel" onClick={()=>setOpen(false)}/>
+                <input className="btn action_button" type="submit" value="Login"
+                style={{ border: "none", borderRadius: "10px", backgroundColor: "#3E5A20", color: "#F5F5F5", padding: "5px 20px",  }}/>
+                <input className="btn action_button" type="button" value="Cancel" onClick={()=>setOpen(false)}
+                style={{ border: "none", borderRadius: "10px", backgroundColor: "#8E0111", color: "#F5F5F5", padding: "5px 20px"}}/>
               </div>
-              <a className="loginlink" href="/register">Register Now</a>
+
+              <a className="loginlink" href="/register" style={{ color: "#F5F5F5", textDecoration: "none" }}>Register Now</a>
           </form>
       </div>
     </div>
