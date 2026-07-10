af9c7f84a313022b17d5d30713f01eba97828687
Jarad Inniss <jjinniss02@hotmail.com>
2026-07-10 09:32:20 -0400

    Added Dynamic Pages

--- server/frontend/src/components/Dealers/Dealer.jsx
+++ server/frontend/src/components/Dealers/Dealer.jsx
@@ -60,9 +60,12 @@ const Dealer = () => {
     get_dealer();
     get_reviews();
     if(sessionStorage.getItem("username")) {
-      setPostReview(<a href={post_review}><img src={review_icon} style={{width:'10%',marginLeft:'10px',marginTop:'10px'}} alt='Post Review'/></a>)
-
-      
+      setPostReview(
+        <a href={post_review} style={{ textDecoration: "none", color: "#F5F5F5", fontSize: "20px", border: "none", borderRadius: "10px", backgroundColor: "#272E20", padding: "20px 40px" }}>
+            <svg style={{ width:'30px', marginRight:'20px'}} xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#f5f5f5" viewBox="0 0 256 256"><path d="M232,96a16,16,0,0,0-16-16H184V48a16,16,0,0,0-16-16H40A16,16,0,0,0,24,48V176a8,8,0,0,0,13,6.22L72,154V184a16,16,0,0,0,16,16h93.59L219,230.22a8,8,0,0,0,5,1.78,8,8,0,0,0,8-8Zm-42.55,89.78a8,8,0,0,0-5-1.78H88V152h80a16,16,0,0,0,16-16V96h32V207.25Z"></path></svg>
+            Write A Review
+        </a>
+        )
     }
   },[]);  
 
@@ -70,19 +73,20 @@ const Dealer = () => {
 return(
   <div style={{margin:"20px"}}>
       <Header/>
-      <div style={{marginTop:"10px"}}>
-      <h1 style={{color:"grey"}}>{dealer.full_name}{postReview}</h1>
-      <h4  style={{color:"grey"}}>{dealer['city']},{dealer['address']}, Zip - {dealer['zip']}, {dealer['state']} </h4>
+      <div style={{ marginTop:"20px" }}>
+      <h2 style={{ color:"#F5F5F5" }}>{dealer.full_name}{postReview}</h2>
+      <h5 style={{color:"#F5F5F5", marginTop: "30px"}}>{dealer['city']},{dealer['address']}, Zip - {dealer['zip']}, {dealer['state']} </h5>
       </div>
       <div class="reviews_panel">
       {reviews.length === 0 && unreviewed === false ? (
-        <text>Loading Reviews....</text>
-      ):  unreviewed === true? <div>No reviews yet! </div> :
+        <text style={{ color:"#F5F5F5" }}>Loading Reviews....</text>
+      ):  unreviewed === true? <div style={{ color:"#F5F5F5" }}>No reviews yet! </div> :
       reviews.map(review => (
         <div className='review_panel'>
-          <img src={senti_icon(review.sentiment)} className="emotion_icon" alt='Sentiment'/>
-          <div className='review'>{review.review}</div>
-          <div className="reviewer">{review.name} {review.car_make} {review.car_model} {review.car_year}</div>
+            <img src={senti_icon(review.sentiment)} className="emotion_icon" alt='Sentiment' style={{ color: "#F5F5F5", fontSize: "20px", border: "none", borderRadius: "10px", backgroundColor: "#3E5A20"}}/>
+            <div className='review' style={{ color:"#F5F5F5" }}>{review.review}</div>
+            <div className="reviewer" style={{ color:"#F5F5F5" }}> User: {review.name}</div>
+            <div className="reviewer" style={{ color:"#F5F5F5" }}> Reviewing: {review.car_make} {review.car_model} {review.car_year}</div>
         </div>
       ))}
     </div>  
